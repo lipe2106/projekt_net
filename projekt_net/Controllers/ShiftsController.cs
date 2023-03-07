d59786d2269a706f282e809d2f3e68c4cce582ff
Lina Petersson <lipe2106@student.miun.se>
2023-03-07 10:19:23 +0100

    Skapat mvc grund med identity

---
+++ projekt_net/Controllers/ShiftsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using projekt_net.Data;
+using projekt_net.Models;
+
+namespace projekt_net.Controllers
+{
+    public class ShiftsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ShiftsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Shifts
+        public async Task<IActionResult> Index()
+        {
+              return _context.Shift != null ? 
+                          View(await _context.Shift.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Shift'  is null.");
+        }
+
+        // GET: Shifts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Shift == null)
+            {
+                return NotFound();
+            }
+
+            var shift = await _context.Shift
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (shift == null)
+            {
+                return NotFound();
+            }
+
+            return View(shift);
+        }
+
+        // GET: Shifts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Shifts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Type,From,To,Available,Sick,Change,User_Id")] Shift shift)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(shift);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shift);
+        }
+
+        // GET: Shifts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Shift == null)
+            {
+                return NotFound();
+            }
+
+            var shift = await _context.Shift.FindAsync(id);
+            if (shift == null)
+            {
+                return NotFound();
+            }
+            return View(shift);
+        }
+
+        // POST: Shifts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Type,From,To,Available,Sick,Change,User_Id")] Shift shift)
+        {
+            if (id != shift.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(shift);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ShiftExists(shift.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shift);
+        }
+
+        // GET: Shifts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Shift == null)
+            {
+                return NotFound();
+            }
+
+            var shift = await _context.Shift
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (shift == null)
+            {
+                return NotFound();
+            }
+
+            return View(shift);
+        }
+
+        // POST: Shifts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Shift == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Shift'  is null.");
+            }
+            var shift = await _context.Shift.FindAsync(id);
+            if (shift != null)
+            {
+                _context.Shift.Remove(shift);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ShiftExists(int id)
+        {
+          return (_context.Shift?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ projekt_net/Models/Shift.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace projekt_net.Models
+{
+    public class Shift
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Typ av pass")]
+        public string? Type { get; set; }
+
+        [Display(Name = "Från")]
+        public DateTime From { get; set; }
+
+        [Display(Name = "Till")]
+        public DateTime To { get; set; }
+
+        [Display(Name = "Ledigt")]
+        public bool Available { get; set; }
+
+        [Display(Name = "Sjuk")]
+        public bool Sick { get; set; }
+
+        [Display(Name = "Byta pass")]
+        public bool Change { get; set; }
+
+        [Display(Name = "Anställd")]
+        public int User_Id { get; set; }
+    }
+}
